339c6507ee19b55715d4dfe875fc09c34827b87d
Henry Blackwell <alecgammon@gmail.com>
2018-12-09 18:24:16 -0800

    website

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,67 +1,35 @@
-/*
- * @name Acceleration Ball Bounce
- * @description Move an ellipse around based on accelerationX and accelerationY values, and bounces when touch the edge of the canvas.
- */
+//Move your mouse to the right slowly
 
-// Position Variables
-var x = 200;
-var y = 200;
- 
-// Speed - Velocity
-var vx = 500;
-var vy = 500;
- 
-// Acceleration
-var ax = 3;
-var ay = 3;
- 
-var vMultiplier = 0.7;
-var bMultiplier = 0.7;
-
-function setup() {
-    createCanvas(600, 500);
-    fill(0);
-}
-
-function draw() {
-    background(255,218);
-    ballMove();
-    ellipse(x, y, 80, 30);
-  ellipse(y, x, 80, 30);
-  triangle(y,x,x,y,y,x);
-  triangle(x,y,50,0,30,50);
+var animation;
+var angle = 0.0;
+var jitter=0.0;
+function setup(){
+  createCanvas (800,600);
+  noStroke();
+  fill(255);
   
-  c = color(255); // Update 'c' with grayscale value
-fill(c); // Use updated 'c' as fill color
 }
 
-function ballMove() {
-
-	ax = accelerationX;
-	ay = accelerationY;
-
-	vx = vx + ay;
-	vy = vy + ax;
-	y = y + vy * vMultiplier; 
-	x = x + vx * vMultiplier;
-
-	// Bounce when touch the edge of the canvas
-	if (x < 0) { 
-		x = 0; 
-		vx = -vx * bMultiplier; 
-	}
- 	if (y < 0) { 
- 		y = 0; 
- 		vy = -vy * bMultiplier; 
- 	}
- 	if (x > width - 20) { 
- 		x = width - 20; 
- 		vx = -vx * bMultiplier; 
- 	}
- 	if (y > height - 20) { 
- 		y = height - 20; 
- 		vy = -vy * bMultiplier; 
- 	}
-	
+function draw(){
+  var x;
+  var y;
+   background(430,220,100);
+  for(x=0; x<mouseX; x=x+10){
+    fill(random(255),random(255),random(255));
+    ellipse(x,0,x,height);
+    for(y=0; y<mouseX; y=y+10){
+      rect(x,y,x,y);
+      
+   if (second() % .05 == 0) {  
+   jitter = random(-0.1, 0.1);
+  }
+  angle = angle + jitter;
+  var c = cos(angle);
+//  translate(width/2, height/2);
+  rotate(c);
+  ellipse(100, 46, 55, 55);  
+triangle(70, 100, 150, 500, 86, 75);
+  }
 }
-
+//animation=animation+0.5;
+}
